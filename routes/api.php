2d5fe89377cf8d4b2135e2762dc74481c45bef68
Ende Sun <1195562121ende@gmail.com>
2024-07-07 00:31:06 +0200

    api expuesta correctamente

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIUserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/v1/user', APIUserController::class);
+
